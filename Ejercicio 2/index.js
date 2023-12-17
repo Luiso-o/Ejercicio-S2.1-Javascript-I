e9af22764c0593ba0671088629d2f5d594e1fe52
Luiso-o <cheportillo@gmail.com>
2023-12-17 10:29:10 +0100

    mejoras

--- Ejercicio 2/index.js
+++ Ejercicio 2/index.js
@@ -1,4 +1,9 @@
 const randomNumber = () =>{
     const numRandom = Math.floor(Math.random() * 100) + 1;
-    return document.getElementById("resultado").innerText = "Tu numero random es: " + numRandom;
+    document.getElementById("resultado").innerText = "Tu numero random es: " + numRandom;
+
+    //salida por consola
+    console.log("Tu numero random es: " + numRandom);
+
+    return numRandom;
 }
