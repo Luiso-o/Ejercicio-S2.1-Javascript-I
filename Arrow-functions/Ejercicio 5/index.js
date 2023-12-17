b34812de2916ebae29244346b511758971ff2084
Luiso-o <cheportillo@gmail.com>
2023-12-17 14:35:35 +0100

    setTimeout

---
+++ Arrow-functions/Ejercicio 5/index.js
@@ -0,0 +1,11 @@
+setTimeout(() => {
+    console.log("Este es un mensaje que se imprimió 3 segundos después de ejecutado el programa");
+}, 3000);
+
+setTimeout(() => {
+    console.log("Este es un mensaje que se imprimió 6 segundos después de ejecutado el programa");
+}, 6000);
+
+setTimeout(() => {
+    console.log("Este es un mensaje que se imprimió 9 segundos después de ejecutado el programa");
+}, 9000);
