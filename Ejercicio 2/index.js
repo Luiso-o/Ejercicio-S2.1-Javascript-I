09c728e8f7dfcdb4e0a970ddc7fc6362a3f33900
Luiso-o <cheportillo@gmail.com>
2023-12-17 10:15:39 +0100

    n

--- Ejercicio 2/index.js
+++ Ejercicio 2/index.js
@@ -1,4 +1,4 @@
-const random = () =>{
+const randomNumber = () =>{
     const numRandom = Math.floor(Math.random() * 100) + 1;
     return document.getElementById("resultado").innerText = "Tu numero random es: " + numRandom;
 }
